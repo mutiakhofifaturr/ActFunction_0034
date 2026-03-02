dd7198782ad38c039d515918b16baf2a2ce198b2
mimyutt <mutiakhofifaturr@gmail.com>
2026-03-02 12:57:52 +0700

    menambahkan include iostream int panjang dan void input

---
+++ function_with_parameter2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+
+void input()
+{
+    cout << "masukkan panjang :";
+    cin >> panjang;
+    cout << "masukkan lebar :";
+    cin >> lebar;
+}
